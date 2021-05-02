2f686a1c7a93bc81af4541f455bc0aeeb9de29b8
W3ndig0u0 <jingxiang.xu@elev.ga.ntig.se>
2021-05-02 13:24:58 +0200

    :fax:

--- app.js
+++ app.js
@@ -9,3 +9,20 @@ function scrollFunction() {
     document.getElementById("header").style.top = "-10%";
   }
 }
+
+
+var text = ['“What did the 5 fingers say to the face. S L A P!”', '“Hey hey hey, smoke weed everyday”'];
+
+textSequence(0);
+function textSequence(i) {
+
+    if (text.length > i) {
+        setTimeout(function() {
+            document.getElementById("sequence").innerHTML = text[i];
+            textSequence(++i);
+        }, 4000);
+
+    } else if (text.length == i) {
+        textSequence(0);
+    }
+}
