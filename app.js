c7c2e4dab75e1ecb70a55193879d50ad01eeae35
W3ndig0u0 <jingxiang.xu@elev.ga.ntig.se>
2021-05-02 03:22:31 +0200

    :whale:

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
   if (document.body.scrollTop > 670 || document.documentElement.scrollTop > 670) {
     document.getElementById("header").style.opacity = "1";
+    document.getElementById("header").style.top = "0";
   } else {
     document.getElementById("header").style.opacity = "0";
+    document.getElementById("header").style.top = "-10%";
   }
 }
